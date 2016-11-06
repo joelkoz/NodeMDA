8c3abbe0e534f5a192f20fc6ad82905108ce86ff
joelkoz <joel.kozikowski@gmail.com>
2016-11-05 22:14:53 -0400

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-var NodeMDA = require('./nodemda/nodemda-core.js');
+const NodeMDA = require('./nodemda/nodemda-core.js');
 NodeMDA.Meta = require("./nodemda/meta/meta-model.js");
 require('./nodemda/meta/meta-validator.js');
 require('./nodemda/gen/code-generator.js');
