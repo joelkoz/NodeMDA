2d1e623de313ac88409591e028dbc77ed6b19058
joelkoz <joel.kozikowski@gmail.com>
2016-11-08 00:19:12 -0500

    Reorganized code

--- plugin-dev/javascript-es6/plugins/Transformations.js
+++ plugin-dev/javascript-es6/plugins/Transformations.js
@@ -5,30 +5,10 @@ var NodeMDA = require("nodemda");
 /*
  * Utility functions to make Javascript template generation easier.
  */
-var Transformations = Transformations || {};
+var Transformations = {};
 
 (function() {
 
-	    /**
-	     * Transforms all associations specified in the metaClass
-	     * into "Array" attributes, returning them as an array of
-	     * NodeMDA.Meta.Attribute objects.
-	     */
-	    var assocsToArrayAttribs = function(metaClass) {
-	    	var attribs = [];
-	    	metaClass.associations.forEach(function(metaAssoc) {
-	    		var otherEnd = metaAssoc.otherEnd; 
-	    	   if (otherEnd._navigable) {
-	    		   var attrib = new NodeMDA.Meta.Attribute(otherEnd.name, otherEnd.type);
-	    		   attrib._multiplicity = otherEnd._multiplicity;
-	    		   attrib._public = otherEnd._public;
-	    		   attrib._comment = otherEnd._comment;
-	    		   attribs.push(attrib);
-	    	   }	
-	    	});
-	    	return attribs;
-	    };
-	
 	    
 	    /**
 	     * In Javascript, associations to other objects are handled as
@@ -39,8 +19,29 @@ var Transformations = Transformations || {};
 	     * list and association list are saved in jsOriginalAttributes and
 	     * jsOriginalAssociations in case they are needed by another plugin.
 	     */
-	    var makeJSAttributeList = function(metaClass) {
-	       var jsAttributeList = metaClass.attributes.concat(assocsToArrayAttribs(metaClass));
+	    function makeJSAttributeList(metaClass) {
+
+		    /**
+		     * Transforms all associations specified in the metaClass
+		     * into "Array" attributes, returning them as an array of
+		     * NodeMDA.Meta.Attribute objects.
+		     */
+		    function assocsToArrayAttribs(metaClass) {
+		    	let attribs = [];
+		    	metaClass.associations.forEach(function(metaAssoc) {
+		    		let otherEnd = metaAssoc.otherEnd; 
+		    	   if (otherEnd._navigable) {
+		    		   let attrib = new NodeMDA.Meta.Attribute(otherEnd.name, otherEnd.type);
+		    		   attrib._multiplicity = otherEnd._multiplicity;
+		    		   attrib._public = otherEnd._public;
+		    		   attrib._comment = otherEnd._comment;
+		    		   attribs.push(attrib);
+		    	   }	
+		    	});
+		    	return attribs;
+		    };
+
+	       let jsAttributeList = metaClass.attributes.concat(assocsToArrayAttribs(metaClass));
 	       metaClass.jsOriginalAttributes = metaClass.attributes;
 	       metaClass.attributes = jsAttributeList;
 	       metaClass.jsOriginalAssociations = metaClass.associations;
@@ -53,9 +54,9 @@ var Transformations = Transformations || {};
 			
 			// And go ahead and make the pseudo attribute list on the associated classes...
 			metaClass.associations.forEach(function(metaAssoc) {
-				var otherEnd = metaAssoc.otherEnd;
+				let otherEnd = metaAssoc.otherEnd;
 				if (otherEnd._navigable) {
-					var subMeta = metaAssoc.otherEnd.type.metaClass;
+					let subMeta = metaAssoc.otherEnd.type.metaClass;
 					if (typeof(subMeta.jsOriginalAttributes) === "undefined") {
 					    makeJSAttributeList(subMeta);
 					}
