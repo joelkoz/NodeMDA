7524ad0025d2636211d6458299fa98eaa1f18985
joelkoz <joel.kozikowski@gmail.com>
2019-01-02 15:41:59 -0500

    goLang
    Experimental support for goLang

---
+++ plugin-dev/golang-gin-gorm/plugins/_helpers/initJSNamespace.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var Handlebars = require("handlebars");
+
+var initJSNamespace = function(varName, namespaceName) {
+
+    if (typeof namespaceName === "function") {
+    	namespaceName = namespaceName.call(this);
+    }
+    if (namespaceName.trim().length === 0) {
+        return '';
+    }
+	var paths = namespaceName.split(".");
+    var result = '';
+    var parentPath = '';
+    for (var i = 0; i < paths.length; i++) {
+    	var nsName = parentPath + paths[i];
+   	    result += varName + '.' + nsName + " = " + varName + '.' + nsName + " || {};\n";
+    	parentPath = parentPath + paths[i] + ".";
+    } // for
+    
+    return new Handlebars.SafeString(result);
+};
+
+module.exports = initJSNamespace;
