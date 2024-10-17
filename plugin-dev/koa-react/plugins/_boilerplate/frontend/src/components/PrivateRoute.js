17d17a2c06c6bfdbdb13abba6bd20cf4b4e92325
Joel Kozikowski <joel.kozikowski@gmail.com>
2024-10-16 20:14:43 -0400

    Bug fixes to koa-react
    Bug fixes after initial testing of koa-react plugin

--- plugin-dev/koa-react/plugins/_boilerplate/frontend/src/components/PrivateRoute.js
+++ plugin-dev/koa-react/plugins/_boilerplate/frontend/src/components/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import apiClient from '../api/apiClient';
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token');
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return apiClient.isAuthenticated() ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
