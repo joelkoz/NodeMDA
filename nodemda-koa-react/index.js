acb32858f514f964ab8d67825bcab240793f5e67
Joel Kozikowski <joel.kozikowski@gmail.com>
2025-01-10 17:19:39 -0500

    Removed old 1.0 version of koa-react plugin

--- nodemda-koa-react/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	type: 'platform',
-	name: 'koa-react',
-	desc: 'Full stack multi-user SOA app for NodeJS using Koa, React, and Mantine'
-};
--- nodemda-koa-react/plugins/_boilerplate/frontend/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import apiClient from '../api/apiClient';
-
-const PrivateRoute = ({ children }) => {
-  return apiClient.isAuthenticated() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
