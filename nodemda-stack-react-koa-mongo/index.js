2dc42c974717050e6c857a41dd1dc650427fc68b
Joel Kozikowski <joel.kozikowski@gmail.com>
2025-01-11 16:39:47 -0500

    FIX: stack-react-koa-mongo had wrong name in module object

--- nodemda-stack-react-koa-mongo/index.js
+++ nodemda-stack-react-koa-mongo/index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	type: 'stack',
-	name: 'stack-react-koa-mongoose',
+	name: 'stack-react-koa-mongo',
 	desc: 'Generates Full Stack app with React front end and Koa + Mongoose back end with docker files',
 	version: 2.1,
 	contributes: [],
