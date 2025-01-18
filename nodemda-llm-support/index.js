6759bc9d8923141c3baaaf1f87b338c028d23666
Joel Kozikowski <joel.kozikowski@gmail.com>
2025-01-18 16:54:54 -0500

    WIP: llm-support plugin for LLM prompts to write new NodeMDA plugins

---
+++ nodemda-llm-support/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	type: 'mixin',
+	name: 'llm-support',
+	desc: 'Contains partials for generating NodeMDA related LLM prompts',
+	version: 2.0,
+	contributes: [ "" ],
+	requires: [	"" ]
+};
